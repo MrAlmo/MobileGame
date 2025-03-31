9a9eda35ef7ce3c66a3cef366b090d9695ca5982
Andriicsfv <mrmastet522@gmail.com>
2025-03-31 15:31:14 +0200

    Save2(Animation)

--- Assets/scripts/move.cs
+++ Assets/scripts/move.cs
@@ -6,19 +6,25 @@ public class move : MonoBehaviour
 {
     [SerializeField] float speed = 1f;
     private Rigidbody2D rb;
-    
+    private Animator animator;
+
     void Start()
     {
-       rb = GetComponent<Rigidbody2D>(); 
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
-    
     void Update()
     {
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveY = Input.GetAxisRaw("Vertical");
         Vector2 move = new Vector2(moveX, moveY);
 
-        rb.velocity = new Vector2(moveX*speed, moveY * speed);
+        rb.velocity = move * speed;
+
+        
+        animator.SetFloat("MoveX", moveX);
+        animator.SetFloat("MoveY", moveY);
+        animator.SetBool("isMoving", move.sqrMagnitude > 0);
     }
 }
