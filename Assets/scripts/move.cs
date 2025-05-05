df020872adf9bcb524d1b4bec8493090d5b3e4b5
Andriicsfv <mrmastet522@gmail.com>
2025-05-05 21:56:21 +0200

    Map

--- Assets/scripts/move.cs
+++ Assets/scripts/move.cs
@@ -4,8 +4,7 @@ using UnityEngine;
 
 public class move : MonoBehaviour
 {
-    [SerializeField] float speed = 1f;
-    [SerializeField] private Talk dialogueSystem;
+    [SerializeField] private float speed = 1f;
     private Rigidbody2D rb;
     private Animator animator;
 
@@ -17,25 +16,18 @@ public class move : MonoBehaviour
 
     void Update()
     {
-
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveY = Input.GetAxisRaw("Vertical");
-        Vector2 move = new Vector2(moveX, moveY);
+        Vector2 move = new Vector2(moveX, moveY).normalized;
 
-        if (dialogueSystem.IsActive == false)
-        {
-            rb.constraints = RigidbodyConstraints2D.None;
-            rb.constraints = RigidbodyConstraints2D.FreezeRotation;
-            rb.velocity = move * speed;
-        }
-        else
-        {
-            rb.constraints = RigidbodyConstraints2D.FreezeAll;
-        }
+        rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+        rb.velocity = move * speed;
 
-        
-        animator.SetFloat("MoveX", moveX);
-        animator.SetFloat("MoveY", moveY);
-        animator.SetBool("isMoving", move.sqrMagnitude > 0);
+        if (animator != null)
+        {
+            animator.SetFloat("MoveX", moveX);
+            animator.SetFloat("MoveY", moveY);
+            animator.SetBool("isMoving", move.sqrMagnitude > 0);
+        }
     }
 }
